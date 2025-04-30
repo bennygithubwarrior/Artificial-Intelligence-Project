0ff08adc838f50d3b325ea6b78158e7097fd8eb4
bennygithubwarrior <2benneh2@gmail.com>
2025-04-30 20:33:34 +0200

    Update Environment.py

--- RoadTraffic Sign Recognition/Environment.py
+++ RoadTraffic Sign Recognition/Environment.py
@@ -1,1 +1,8 @@
-!pip install tensorflow opencv-python scikit-learn
+!pip install tensorflow opencv-python scikit-learn #Installing Dependencies
+
+from google.colab import drive 
+drive.mount('/content/drive')   #Mounting Drive
+
+!unzip /content/drive/MyDrive/GTSRB-Training_fixed.zip -d gtsrb #unzipping the dataset
+
+
