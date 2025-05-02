6d6a4c15311ed85022586acabb91eecedbe668e5
bennygithubwarrior <2benneh2@gmail.com>
2025-05-02 20:41:02 +0200

    Delete RoadTraffic Sign Recognition directory

--- RoadTraffic Sign Recognition/Environment.py
+++
@@ -1,8 +0,0 @@
-!pip install tensorflow opencv-python scikit-learn #Installing Dependencies
-
-from google.colab import drive 
-drive.mount('/content/drive')   #Mounting Drive
-
-!unzip /content/drive/MyDrive/GTSRB-Training_fixed.zip -d gtsrb #unzipping the dataset
-
-
--- RoadTraffic Sign Recognition/ModelArchitecture.py
+++
@@ -1,3 +0,0 @@
-# Displaying the model's information
-
-model.summary()
